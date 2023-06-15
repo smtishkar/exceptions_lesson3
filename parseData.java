adf3e7196c90f2fbd75927f1bad3f89627c49a08
Sergey <smtishkar@mail.ru>
2023-06-15 11:42:25 +0300

    удалил метод который не использовался

--- parseData.java
+++ parseData.java
@@ -6,7 +6,7 @@ public class parseData {
 
     public static String[] parsingOfData(String[] arr) {
         try {
-            LocalDate.parse(arr[3], DateTimeFormatter.ofPattern("MM.dd.yyyy"));
+            LocalDate.parse(arr[3], DateTimeFormatter.ofPattern("dd.MM.yyyy"));
             Long.parseLong(arr[4]);
         } catch (RuntimeException e) {
             System.out.println(e + " Данные введены не в корректном формате (телефон или дата рождения)");
